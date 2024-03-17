da987045f49bd66b1366080f90658b1375f6eac2
jeevanshajujohn <jeevanshajujohn@gmail.com>
2024-03-17 13:01:04 +0530

    Function for checking if any given String works for a given NFA updated and working

--- src/AutomataNodes/RelationFunctions.java
+++ src/AutomataNodes/RelationFunctions.java
@@ -3,22 +3,24 @@ package AutomataNodes;
 import java.util.Arrays;
 
 public class RelationFunctions{
+    static boolean check = false;
     public static boolean checkIfValidString(String initString) {
         char[] splitString = initString.toCharArray();
         for(char element:splitString){
-            if(TransitionList.checkIfInTransitionList(element))
+            if(!TransitionList.checkIfInTransitionList(element))
                 return false;
         }
         return recursiveChecker(splitString, NFA.initialState);
     }
     public static boolean recursiveChecker(char[] arr, State preState){
-        boolean check = false;
         if(arr.length == 1){
             for (Relation rel : RelationsList.relationsList) {
                 if(rel.fromState.equals(preState)) {
-                    if (Arrays.stream(NFA.finalState).anyMatch(n -> n == rel.toState)){
-                        if(new Transition(String.valueOf(arr[0])).equals(rel.connection)) {
-                            return true;
+                    for(State state: NFA.finalState){
+                        if(state.equals(rel.toState)){
+                            if(new Transition(String.valueOf(arr[0])).equals(rel.connection)) {
+                                return true;
+                            }
                         }
                     }
                 }
--- src/AutomataNodes/Transition.java
+++ src/AutomataNodes/Transition.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Objects;
 
 public class Transition {
+    String data;
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -16,8 +17,6 @@ public class Transition {
     public int hashCode() {
         return Objects.hash(data);
     }
-
-    String data;
 
     Transition(String nextInput) {
         data = nextInput.toLowerCase(Locale.of(nextInput));
